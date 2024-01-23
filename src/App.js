085350b1387b85b8f8e614a627a8d97813d11eb7
Jake Byford <65736328+jakebyford@users.noreply.github.com>
2024-01-23 17:16:32 -0500

    update for v3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-	  return (
-		  		      <div className="App">
-		  		        <header className="App-header">
-		  		          <img src={logo} className="App-logo" alt="logo" />
-		  		          <h1>Hello from V2</h1>
-		  		        </header>
-		  		      </div>
-		  		    );
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
@@ -34,6 +21,5 @@ function App({ signOut }) {
 		    );
 }
 
-export default App;
 export default withAuthenticator(App);
 
